5e4cfbcb8c48c1bf5ed2fbedaffcef2aa37c7531
Heon2 <adlgjs@naver.com>
2022-01-06 19:19:25 +0900

    완주하지못한선수.py

--- hash/완주하지 못한 선수.py
+++ hash/완주하지 못한 선수.py
@@ -1,2 +1,7 @@
-def solution(participant, completion):
-    return answer
+def solution(participant, completion): #participant = ["leo", "kiki", "eden"], completion = ["eden", "kiki"]
+    participant.sort()             #['eden', 'kiki', 'leo']
+    completion.sort()              #['eden', 'kiki']
+    completion.append('')          #['eden', 'kiki', '']
+    for p,c in zip(participant,completion):
+        if p!=c:
+            return p
